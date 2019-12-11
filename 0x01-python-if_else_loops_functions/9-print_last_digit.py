6e303a8f6696663bb4a9ee10bb6278ac8a3aebce
waffle <asuka9767@protonmail.com>
2019-12-11 07:38:51 +0000

    changed method to abs

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    num = str(number)[-1:]
-    print("{}".format(num), end="")
-    return (int(num))
+    digit = abs(number) % 10
+    print("{}".format(digit), end="")
+    return ((digit))
