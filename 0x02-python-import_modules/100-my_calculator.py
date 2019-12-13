eb850d2d47b9c0aebcd0681e763bbbf1d1d9b6fd
waffle <asuka9767@protonmail.com>
2019-12-13 08:41:51 +0000

    fixed last case to slash

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
 
-    if (len(sys.argv) - 1) != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     result = 0
     if word is '+':
-        result = calculator_1.add(a, b)
+        result = add(a, b)
     elif word is '-':
-        result = calculator_1.sub(a, b)
+        result = sub(a, b)
     elif word is '*':
-        result = calculator_1.mul(a, b)
-    elif word is '*':
-        result = calculator_1.div(a, b)
+        result = mul(a, b)
+    elif word is '/':
+        result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
