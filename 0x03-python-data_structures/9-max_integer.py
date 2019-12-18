cada8e7e70d12396349ec4283ab83a04dded536e
waffle <asuka9767@protonmail.com>
2019-12-18 05:13:02 +0000

    uploading for testing

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+    else:
+        biggest = 0
+        length = len(my_list)
+        for i in range(length):
+            if biggest < my_list[i]:
+                biggest = my_list[i]
+        return (biggest)
