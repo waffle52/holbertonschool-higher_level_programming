7d474dc924b27d699093169fb54838c97727e706
waffle <asuka9767@protonmail.com>
2019-12-18 16:44:59 +0000

    added test case for none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return (None)
     length = len(my_list)
     my_list.reverse()
     for i in range(0, length):
