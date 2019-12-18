65974e2db84aedad81f2b2503af569d98a04015d
waffle <asuka9767@protonmail.com>
2019-12-18 08:34:15 +0000

    set var to 0th index

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@ def max_integer(my_list=[]):
     if not my_list:
         return (None)
     else:
-        biggest = my_list[1]
+        biggest = my_list[0]
         length = len(my_list)
         for i in range(length):
-            if biggest <= my_list[i]:
+            if biggest < my_list[i]:
                 biggest = my_list[i]
         return (biggest)
