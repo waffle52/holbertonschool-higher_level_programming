9a7638a7690b8729c3b81d6877c3f44fb2725f7d
waffle <asuka9767@protonmail.com>
2019-12-19 23:27:04 +0000

    removed test case

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return None
-    else:
-        new_mat = [x[:] for x in matrix]
-        for i in range(3):
-            for j in range(3):
-                new_mat[i][j] = new_mat[i][j] ** 2
-        return (new_mat)
+    new_mat = [x[:] for x in matrix]
+    for i in range(3):
+        for j in range(3):
+            new_mat[i][j] = new_mat[i][j] ** 2
+    return (new_mat)
