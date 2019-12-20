01bb0b66138716f1882e42b8d8e628229ed5b03f
waffle <asuka9767@protonmail.com>
2019-12-20 00:01:25 +0000

    added length based of given matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_mat = [x[:] for x in matrix]
-    for i in range(3):
-        for j in range(3):
+    length = len(new_mat)
+    for i in range(0, length):
+        for j in range(0, length):
             new_mat[i][j] = new_mat[i][j] ** 2
     return (new_mat)
