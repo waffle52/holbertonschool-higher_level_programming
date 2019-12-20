01e036c5471fba094b9f5f0fba0895c9a8f5ad85
waffle <asuka9767@protonmail.com>
2019-12-20 20:31:19 +0000

    copied to new dictionary and traversed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pthon3
 def multiply_by_2(a_dictionary):
-    new_list = {}
-    for x, y in a_dictionary.items():
-        new_list[x] = y * 2
-    return (new_list)
+    new_dict = a_dictionary.copy()
+    for x, y in new_dict.items():
+        new_dict[x] = y * 2
+    return (new_dict)
