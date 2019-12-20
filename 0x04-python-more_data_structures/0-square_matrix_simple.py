bfdd9fd0d591ff520dd5421f0264db6ecd4a313f
waffle <asuka9767@protonmail.com>
2019-12-20 03:57:12 +0000

    changed to use map function and loop through matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_mat = [x[:] for x in matrix]
-    length = len(new_mat)
-    for i in range(0, length):
-        for j in range(0, length):
-            new_mat[i][j] = new_mat[i][j] ** 2
-    return (new_mat)
+        new_list = [list(map(lambda x: x**2, i)) for i in matrix]
+
+        return (new_list)
