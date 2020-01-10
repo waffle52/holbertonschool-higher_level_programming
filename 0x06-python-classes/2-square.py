2a34ba206c992bdd65bee0d478fd9c42436de77d
waffle <asuka9767@protonmail.com>
2020-01-10 08:14:36 +0000

    forgot to add 3 to shebang

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 class Square:
     """class Square"""
     def __init__(self, size=0):
         """def init"""
         try:
-            self._Square__size = size
-            if size < 0:
-                raise ValueError
+            if isinstance(size, int):
+                self._Square__size = size
+            else:
+                raise TypeError
         except TypeError:
             print("size must be an integer")
         except ValueError:
