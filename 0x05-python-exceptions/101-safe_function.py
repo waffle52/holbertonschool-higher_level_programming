50259b59f2fb1a192a2e69b0e2903852b2823b50
waffle <asuka9767@protonmail.com>
2020-01-12 19:52:21 +0000

    changed to call with args overall

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return(fct(args[0], args[1]))
+        return(fct(*args))
     except Exception as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return (None)
