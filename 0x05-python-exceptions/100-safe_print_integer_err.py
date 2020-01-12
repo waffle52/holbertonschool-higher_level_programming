806fb96223c37bdebddbbef797d1cc006a14557a
waffle <asuka9767@protonmail.com>
2020-01-12 01:58:00 +0000

    added exception for TypeError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError as ex:
+    except (ValueError, TypeError) as ex:
         print(ex, file=sys.stderr)
         return (False)
