4c72311bc4d7478bd41954deadbaddd0c67c7f74
waffle <asuka9767@protonmail.com>
2020-01-12 20:47:07 +0000

    changed to raise error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,10 @@ class Square:
     """class Square"""
     def __init__(self, size=0):
         """def init"""
-        try:
-            self.__size = size
-            if type(size) is not int:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        except TypeError:
-            pass
+        self.__size = size
+        if type(size) is not int:
             print("size must be an integer")
-        except ValueError:
+            raise TypeError
+        elif size < 0:
             print("size must be >= 0")
+            raise ValueError
