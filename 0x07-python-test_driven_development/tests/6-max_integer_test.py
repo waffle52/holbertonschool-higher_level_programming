075cdf86069921f2ce9a206a5b2563a94a730324
waffle <asuka9767@protonmail.com>
2020-01-14 06:29:02 +0000

    fixed to pep8 standard

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,19 +4,24 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+
     def test_list(self):
         my_list = [5, 10, 9, 7]
         # tests for biggest num in list
         self.assertEqual(max_integer(my_list), 10)
+
     def test_empty(self):
         my_list = []
         # tests if list is empty
         self.assertEqual(max_integer(my_list), None)
+
     def test_oneValue(self):
         my_list = [10]
         # tests if list has only one value
         self.assertEqual(max_integer(my_list), 10)
+
     def test_float(self):
         my_list = [10, 15, 16.5]
         # tests if will return float
