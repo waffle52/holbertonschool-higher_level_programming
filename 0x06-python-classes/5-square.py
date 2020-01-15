6cfcbab2e071a6a631d2116d7155cbae4bcafad5
waffle <asuka9767@protonmail.com>
2020-01-15 09:15:34 +0000

    uploading for testing

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        """def init"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """def area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """ prints Square """
+        if self.__size == 0:
+            print("")
+        else:
+            for j in range(self.size):
+                for i in range(self.size):
+                    print("#", end="")
+                print("\n")
