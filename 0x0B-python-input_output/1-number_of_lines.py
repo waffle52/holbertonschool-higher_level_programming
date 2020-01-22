c3dafc3747c8fa4171f7223b669e5d9cff808fdc
waffle <asuka9767@protonmail.com>
2020-01-22 04:00:20 +0000

    changed from loop to len count

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,6 +2,5 @@
 def number_of_lines(filename=""):
     """ func to get number of lines in passed file """
     with open(filename, encoding="utf=8") as MyFile:
-        for i, l in enumerate(MyFile):
-            pass
-        return (i + 1)
+        count = len(MyFile.readlines())
+        return (count)
