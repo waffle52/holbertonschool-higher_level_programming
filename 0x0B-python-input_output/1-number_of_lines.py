6415ef3bd114ace6f86f13875e319a99d8eab307
waffle <asuka9767@protonmail.com>
2020-01-22 03:58:30 +0000

    added encoding

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     """ func to get number of lines in passed file """
-    with open(filename) as MyFile:
+    with open(filename, encoding="utf=8") as MyFile:
         for i, l in enumerate(MyFile):
             pass
         return (i + 1)
