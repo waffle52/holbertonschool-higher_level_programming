8944f2aceb6771c723bb93dcdca534d5037e4a21
waffle <asuka9767@protonmail.com>
2020-01-22 05:58:05 +0000

    added end to print

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,7 +3,7 @@ def read_lines(filename="", nb_lines=0):
     """ func to print file as whole or by line limit """
     with open(filename, encoding="utf-8") as MyFile:
         if nb_lines <= 0 or nb_lines >= len(MyFile.readlines()):
-            print(MyFile.read())
+            print(MyFile.read(), end="")
         else:
             MyFile.seek(0)
             i = 1
