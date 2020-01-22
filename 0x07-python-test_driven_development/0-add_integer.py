4cba004442989f592dc567234517e18a78249b82
waffle <asuka9767@protonmail.com>
2020-01-22 16:05:22 +0000

    changed conditional to is for type None

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """ Returns addition of both integers
     >>> result = add_integer(10, 15)
     """
-    if a == None or b == None:
+    if a is None or b is None:
         return None
     if type(a) == float or type(b) == float:
         a = int(a)
