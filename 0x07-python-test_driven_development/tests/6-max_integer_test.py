cc110baf85beacf4f45c635fd98f3f1f0341aec9
waffle <asuka9767@protonmail.com>
2020-01-24 05:32:15 +0000

    added more tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -26,3 +26,21 @@ class TestMaxInteger(unittest.TestCase):
         my_list = [10, 15, 16.5]
         # tests if will return float
         self.assertEqual(max_integer(my_list), 16.5)
+
+    def test_negNum(self):
+        my_list = [10, -5, 15]
+        # tests if list has a negative number
+        self.assertEqual(max_integer(my_list), 15)
+
+    def test_allNegNum(self):
+        my_list = [-10, -5, -20]
+        # tests if list contains only negative ints
+        self.assertEqual(max_integer(my_list), -5)
+
+    def test_maxFirst(self):
+        my_list = [20, 10, 5]
+        self.assertEqual(max_integer(my_list), 20)
+
+    def test_maxEnd(self):
+        my_list = [10, 5, 20]
+        self.assertEqual(max_integer(my_list), 20)
