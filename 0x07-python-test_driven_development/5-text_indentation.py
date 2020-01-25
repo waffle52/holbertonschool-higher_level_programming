08eca5238dc298a2ed9cfa5794e3c9e9bbce0aa1
waffle <asuka9767@protonmail.com>
2020-01-25 01:17:58 +0000

    rewrote program to use replace method

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 """
-
 Module that seperates text based on chars
-
 """
 
 
 def text_indentation(text):
     """
-    >>> text_indentation("This text should go to new line. hello therex")
+    >>> text_indentation("This text should go to new line. hello there")
     """
     if text is None:
         return None
     if type(text) != str:
         raise TypeError("text must be a string")
-    it = iter(text)
-    for i in it:
-        if (i == '.' or i == '?' or i == ':'):
-            print("{}".format(i), end="")
-            print("\n")
-            next(it)
-        else:
-            print("{}".format(i), end="")
+    new = '\n\n'
+    text = text.replace('.', '.' + new)
+    text = text.replace('?', '?' + new)
+    text = text.replace(':', ':' + new)
+
+    print("\n".join([x.strip() for x in text.split("\n")]), end="")
