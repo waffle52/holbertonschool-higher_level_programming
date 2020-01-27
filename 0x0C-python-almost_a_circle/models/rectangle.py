d90021428b57e5a905af3278128949509dcf826c
waffle <asuka9767@protonmail.com>
2020-01-27 22:30:09 +0000

    Added documentation for class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-
+    """ Class Rectangle """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Init function """
         super().__init__(id)
