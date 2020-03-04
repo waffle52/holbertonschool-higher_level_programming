438727bca2c6627ba048dcd81bf07b0eff740d3e
waffle <asuka9767@protonmail.com>
2020-03-04 02:10:27 +0000

    uploading for testing

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+       identified by 'user_0d_1_pwd';
+GRANT ALL
+      ON * . *
+      TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
