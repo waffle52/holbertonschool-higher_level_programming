167e09fb80c0e57a5c35f4aa268c4267c6d7e5c4
waffle <asuka9767@protonmail.com>
2020-03-04 04:11:00 +0000

    uploading for testing

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
+GRANT SELECT
+      ON * . *
+      TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
