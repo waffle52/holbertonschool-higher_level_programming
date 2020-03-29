e89a487aa755a59b5a8d24c8c4d07ac470932dec
waffle <asuka9767@protonmail.com>
2020-03-29 03:47:46 +0000

    Fixed to pep8 standard

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY states.id ASC;"
-                .format(sys.argv[4]))
+    com = ("""SELECT * FROM states WHERE BINARY name='{}'
+    ORDER BY states.id ASC;""".format(sys.argv[4]))
+    cur.execute(com)
     result = cur.fetchall()
 
     for x in result:
