e8725f0a1e16adde48bf953f08f513af5e761cff
waffle52 <873@holbertonschool.com>
2020-03-29 20:00:21 +0000

    Changed to save arg to variable and then pass in execute

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    com = "SELECT * FROM states WHERE BINARY name='%s' ORDER BY states.id ASC;"
-    cur.execute(com % sys.argv[4])
+    arg = sys.argv[4]
+    cur.execute("""SELECT * FROM states WHERE BINARY name = %s ORDER BY
+    states.id ASC;""", (arg,))
     result = cur.fetchall()
 
     for x in result:
