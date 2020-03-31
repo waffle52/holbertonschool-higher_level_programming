f83c351bdcd1a333d3dc2f60aeebf24a9af88b44
waffle52 <873@holbertonschool.com>
2020-03-31 04:58:35 +0000

    Uploading for testing

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument """
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import asc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+    to_add = session.query(State).filter(State.id == '2').first()
+    to_add.name = "New Mexico"
+    session.commit()
+
+    session.close()
