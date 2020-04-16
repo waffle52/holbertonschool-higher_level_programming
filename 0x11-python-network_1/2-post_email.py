b867965ef9e7ac5bec886e1b73906ca4abec4f7a
waffle <asuka9767@protonmail.com>
2020-04-16 04:22:17 +0000

    forgot to add print statement for result

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email' : sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
+    data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         html = response.read()
+        print("{}".format(html.decode('utf-8')))
