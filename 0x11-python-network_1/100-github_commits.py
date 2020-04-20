2a820ed90ec6abf58eb1694c9c9b1f2c520f5663
waffle <asuka9767@protonmail.com>
2020-04-20 01:56:53 +0000

    Added len to work if length is less than 10 asked for

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script to """
+""" Script to print 10 recent commits with sha and author name """
 import requests
 import sys
 
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[1], sys.argv[2])
     r = requests.get(url)
+    y = 10
     data = r.json()
-    for x in range(0, 10):
+    if (len(data) < y):
+        y = len(data)
+    for x in range(0, y):
         print("{}: {}".format(data[x]['sha'],
                               data[x]['commit']['author']['name']))
