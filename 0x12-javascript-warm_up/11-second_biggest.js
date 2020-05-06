b6f907ede2de1ece6954ac06bedf24ba7bf74522
waffle52 <873@holbertonschool.com>
2020-05-06 04:45:54 +0000

    added parseint to condtional

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (myArgs.length === 0 || myArgs.length === 1) {
   let biggest = parseInt(myArgs[0], 10);
   let nextBig = parseInt(myArgs[0], 10);
   for (let i = 0; i < myArgs.length; i++) {
-    if (myArgs[i] > biggest) {
+    if (parseInt(myArgs[i], 10) > biggest) {
       nextBig = parseInt(biggest, 10);
       biggest = parseInt(myArgs[i], 10);
     } else if (parseInt(myArgs[i], 10) > nextBig && parseInt(myArgs[i], 10) !== biggest) {
