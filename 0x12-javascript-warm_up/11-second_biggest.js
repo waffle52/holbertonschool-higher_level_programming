33bc6897a2ac8737adf9f35d300db7802982b3ec
waffle52 <873@holbertonschool.com>
2020-05-06 04:38:07 +0000

    Added parseint to every number assigned

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,14 @@ const myArgs = process.argv.slice(2);
 if (myArgs.length === 0 || myArgs.length === 1) {
   console.log('0');
 } else {
-  let biggest = myArgs[0];
-  let nextBig = myArgs[0];
+  let biggest = parseInt(myArgs[0], 10);
+  let nextBig = parseInt(myArgs[0], 10);
   for (let i = 0; i < myArgs.length; i++) {
     if (myArgs[i] > biggest) {
-      nextBig = biggest;
-      biggest = myArgs[i];
-    } else if (myArgs[i] > nextBig && myArgs[i] !== biggest) {
-      nextBig = myArgs[i];
+      nextBig = parseInt(biggest, 10);
+      biggest = parseInt(myArgs[i], 10);
+    } else if (parseInt(myArgs[i], 10) > nextBig && parseInt(myArgs[i], 10) !== biggest) {
+      nextBig = parseInt(myArgs[i], 10);
     }
   }
   console.log(nextBig);
