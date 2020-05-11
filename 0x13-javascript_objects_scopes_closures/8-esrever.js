1a1f71cf81f93b30c2620c566191010f304bac7e
waffle52 <873@holbertonschool.com>
2020-05-11 22:36:17 +0000

    Subtracted 1 from length in loop

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const rev = [];
-  for (let i = list.length; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     rev.push(list[i]);
   }
   return rev;
