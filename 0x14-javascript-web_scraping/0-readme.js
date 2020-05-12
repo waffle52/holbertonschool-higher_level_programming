e2481d93354346441bf395ec08328df170cc40cd
waffle52 <asuka9767@protonmail.com>
2020-05-12 19:57:57 +0000

    added utf8 and return statement

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-'use strict';
 const fs = require('fs');
 const filename = process.argv[2];
 fs.readFile(filename, 'utf8', (error, data) => {
   if (error) {
     console.log(error);
-  } else {
-    console.log(data.toString());
+    return;
   }
+  console.log(data.toString());
 });
