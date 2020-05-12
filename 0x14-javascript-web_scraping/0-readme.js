a47fdd52aa2cf1963c5fbb02839f37604e5c1fa6
waffle52 <asuka9767@protonmail.com>
2020-05-12 19:11:38 +0000

    fixed utf8

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filename = process.argv[2];
-fs.readFile(filename, "utf-8", (err, data) => {
+fs.readFile(filename, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
