c9f8c596ecd5994e9f9dd426199efa60d4dbe70d
waffle52 <asuka9767@protonmail.com>
2020-05-12 20:58:36 +0000

    Rewrote to be smaller and use includes to find string

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,14 @@
 const request = require('request');
 const url = process.argv[2];
 let jsonObject;
-let length = 0;
-let secLength = 0;
 let total = 0;
-const checkUrl = 'https://swapi-api.hbtn.io/api/people/18/';
 request.get(url, 'utf8', function (err, response, body) {
   if (err) throw err;
   jsonObject = JSON.parse(body);
-  length = jsonObject.count;
-  for (let x = 0; x < length; x++) {
-    secLength = jsonObject.results[x].characters;
-    for (let y = 0; y < secLength.length; y++) {
-      if (secLength[y] === checkUrl) {
-        total = total + 1;
-      }
+  for (let y = 0; y < jsonObject.results.length; y++) {
+    const characters = jsonObject.results[y].characters;
+    if (characters.filter(str => str.includes('people/18/')).length !== 0) {
+      total++;
     }
   }
   console.log(total);
