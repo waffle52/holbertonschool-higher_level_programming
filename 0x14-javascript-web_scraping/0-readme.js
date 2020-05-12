6cd16ec09b1a710dadd6987b8cd24b879627cec9
waffle52 <873@holbertonschool.com>
2020-05-12 08:08:03 +0000

    Uploading for testing

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+// let content = fs.readFileSync(process.cwd() + '/' + filename).toString();
+fs.readFile(filename, (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data.toString());
+  }
+});
+// console.log(content);
