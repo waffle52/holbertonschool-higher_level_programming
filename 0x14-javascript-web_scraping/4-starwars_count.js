c600ca4947f56a6b9237631847c2a24ba67e12a1
waffle52 <asuka9767@protonmail.com>
2020-05-12 20:42:22 +0000

    Uploading for testing

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let jsonObject;
+let length = 0;
+let secLength = 0;
+let total = 0;
+const checkUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+request.get(url, 'uf8', function (err, response, body) {
+  if (err) throw err;
+  jsonObject = JSON.parse(body);
+  length = jsonObject.count;
+  for (let x = 0; x < length; x++) {
+    secLength = jsonObject.results[x].characters;
+    for (let y = 0; y < secLength.length; y++) {
+      if (secLength[y] === checkUrl) {
+        total = total + 1;
+      }
+    }
+    // console.log(jsonObject['results'][x]['characters'].length);
+  }
+  console.log(total);
+});
