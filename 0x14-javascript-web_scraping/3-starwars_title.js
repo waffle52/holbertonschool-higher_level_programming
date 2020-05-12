0c0f0dcb5c809407bd3c3778f1ad9e0598dbeecd
waffle52 <asuka9767@protonmail.com>
2020-05-12 20:09:08 +0000

    Added utf8

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const id = process.argv[2];
 let url = 'https://swapi-api.hbtn.io/api/films/';
 url = url.concat(id);
 let jsonObject;
-request(url, function (err, response, body) {
+request(url, 'utf8', function (err, response, body) {
   if (err) throw err;
   jsonObject = JSON.parse(body);
   console.log(jsonObject.title);
