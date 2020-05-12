ad52c281eae8882b41d6b579de5bc709ec5835c7
waffle52 <asuka9767@protonmail.com>
2020-05-12 19:57:30 +0000

    changed to

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+'use strict';
 const fs = require('fs');
 const filename = process.argv[2];
-fs.readFile(filename, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
+fs.readFile(filename, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data.toString());
   }
