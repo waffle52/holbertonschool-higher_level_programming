dec2a9aab28f2430a5826c018265b82a5679b967
waffle52 <asuka9767@protonmail.com>
2020-05-12 18:34:57 +0000

    Uploading for testing

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, response) {
+  if (err) throw err;
+  console.log('code:', response && response.statusCode);
+});
